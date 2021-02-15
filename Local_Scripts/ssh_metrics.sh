81ea516fca6dd62427851bc415605a205872c00d
TRenn <u1169633@utah.edu>
2021-02-15 16:55:08 -0700

    Update ssh_metrics.sh
    added functionality for multiple ue/enbs

--- Local_Scripts/ssh_metrics.sh
+++ Local_Scripts/ssh_metrics.sh
@@ -7,10 +7,18 @@
 # Read/Declare vars
 read -p "POWDER username: " UNAME
 read -a PC -p "PC #(s): "
-read -a LOC -p "UE location(s): " LOC
+read -a LOC -p "UE location(s): "
 
-ADDR_UE="${UNAME}@nuc2.${LOC}.powderwireless.net"
-ADDR_ENB="${UNAME}@pc${PCNUM}-fort.emulab.net"
+ADDR_UE=()
+ADDR_ENB=()
+for PC in "${PC[@]}"; do
+  ADDR_ENB="${UNAME}@pc${PCNUM}-fort.emulab.net"
+done
+
+for LOC in "${LOC[@]}"; do
+  ADDR_UE="${UNAME}@nuc2.${LOC}.powderwireless.net"
+done
+
 BASE_DIR="/proj/mmpn-PG0/"
 
 # MAIN
@@ -20,20 +28,24 @@ ssh -p22 ${ADDR_ENB} 'sudo pkill -x srsepc; sudo pkill -x srsenb'
 ssh -p22 ${ADDR_UE} 'sudo pkill -x srsue'
 
 # ################### STEP 1: Start srsLTE ####################################
-# Step 1.1 Start EPC
-tput setaf 6
-echo "Connecting to EPC at ${ADDR_ENB}..."
-tput sgr0
-gnome-terminal --tab -- bash -ic "ssh -p22 ${ADDR_ENB} 'sudo srsepc'; exec bash"
+for ADDRS in "${ADDR_ENB[@]}"; do
+  # Step 1.1 Start EPC
+  tput setaf 6
+  echo "Connecting to EPC at ${ADDRS}..."
+  tput sgr0
+  gnome-terminal --tab -- bash -ic "ssh -p22 ${ADDRS} 'sudo srsepc'; exec bash"
 
-# Step 1.2 Start eNB
-tput setaf 6
-echo "Connecting to eNB at ${ADDR_ENB}..."
-tput sgr0
-gnome-terminal --tab -- bash -ic "ssh -p22 ${ADDR_ENB} -Y 'cd ${BASE_DIR}'; exec bash"
+  # Step 1.2 Start eNB
+  tput setaf 6
+  echo "Connecting to eNB at ${ADDRS}..."
+  tput sgr0
+  gnome-terminal --tab -- bash -ic "ssh -p22 ${ADDRS} -Y 'cd ${BASE_DIR}'; exec bash"
+done
 
-# Step 1.3 Start UE
-tput setaf 6
-echo "Connecting to UE at ${ADDR_UE}..."
-tput sgr0
-gnome-terminal --tab -- bash -ic "ssh -p22 ${ADDR_UE} -Y 'cd ${BASE_DIR}'; exec bash"
+for ADDRS in "${ADDR_UE[@]}"; do
+  # Step 1.3 Start UE
+  tput setaf 6
+  echo "Connecting to UE at ${ADDRS}..."
+  tput sgr0
+  gnome-terminal --tab -- bash -ic "ssh -p22 ${ADDRS} -Y 'cd ${BASE_DIR}'; exec bash"
+done
