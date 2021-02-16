1da72e115ef742763c58551e5081491f7f841036
TRenn <u1169633@utah.edu>
2021-02-15 17:58:49 -0700

    Update kafka_start.sh

--- Node_Scripts/kafka_start.sh
+++ Node_Scripts/kafka_start.sh
@@ -9,27 +9,27 @@
 
 ############################ Variables #########################
 HOST_FILE="/etc/hosts"
-KAF_FILE="/proj/mmpn-pg1/Kafka"
+KAF_FILE="/proj/mmpn-PG1/Kafka"
 
 ############################ Step 1 ############################
 read -p "Kafka Server IP: " KAF_IP
 
 ############################ Step 2 ############################
 grep -q '^${KAF_IP}' ${HOST_FILE} \
-  || echo -e "${KAF_IP} \t DESKTOP-B04CIKU" ${HOST_FILE}
+  || echo -e "${KAF_IP} \t DESKTOP-B04CIKU" >> ${HOST_FILE}
 
 ############################ Step 3 ############################
 IFS='.' read -a NODE_ID <<< $(hostname)
 
 ############################ Step 4 ############################
-sed -i "s/^bootsrap.*/bootstrap.servers=${KAF_IP}:9092" \
+sed -i "s/^bootsrap.*/bootstrap.servers=${KAF_IP}:9092/" \
   ${KAF_FILE}/config/producer.properties
 
 ############################ Step 5 ############################
 tail -f -n0 srslte.log | \
-  .${KAF_FILE}/bin/producer.sh --topic ${NODE_ID}_log --bootstrap-server=${KAF_IP}:9092 &
+  .${KAF_FILE}/bin/kafka-console-producer.sh --topic ${NODE_ID}_log --bootstrap-server=${KAF_IP}:9092 &
 
 if [[ ${1} -eq "ue" ]]; then
   tail -f -n0 /tmp/ue_metrics.csv | \
-    .${KAF_FILE}/bin/producer.sh --topic ${NODE_ID}_metrics --bootstrap-server=${KAF_IP}:9092 &
+    .${KAF_FILE}/bin/kafka-console-producer.sh --topic ${NODE_ID}_metrics --bootstrap-server=${KAF_IP}:9092 &
 fi
