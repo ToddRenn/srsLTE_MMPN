c0a289d112c1512a660be6618287d42d53ff492a
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-03-04 12:57:27 -0700

    Update kafka_create_topics.sh

--- Node_Scripts/Kafka_Scripts/kafka_create_topics.sh
+++ Node_Scripts/Kafka_Scripts/kafka_create_topics.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # This script creates a topic on the node Kafka server
+BASE="/proj/MMPN/groups/PG0/srsLTE_MMPN"
 
 read -a topicName -p "Enter topic name(s): "
 NODE_IP=$(ifconfig eno1 | grep -Po 'inet \K[\d.]+')
 
 for name in "${topicName[@]}"; do
 	echo "Creating topic: "${name}
-	../../Kafka/bin/kafka-topics.sh --create --topic ${name} --bootstrap-server ${NODE_IP}:9092
+	${BASE}/Kafka/bin/kafka-topics.sh --create --topic ${name} --bootstrap-server ${NODE_IP}:9092
 done
