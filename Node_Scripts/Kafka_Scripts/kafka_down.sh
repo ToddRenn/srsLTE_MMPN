375d113a064ed47b1120ad0994abdf14c0d16c6e
Todd Renner <T_Renner@node.t-renner-qv93181.mmpn.emulab.net>
2021-03-04 12:27:39 -0700

    added termination messages for kafka_down.sh

--- Node_Scripts/Kafka_Scripts/kafka_down.sh
+++ Node_Scripts/Kafka_Scripts/kafka_down.sh
@@ -3,5 +3,6 @@
 BASE="/proj/MMPN/groups/PG0/srsLTE_MMPN"
 
 ${BASE}/Kafka/bin/zookeeper-server-stop.sh
-
+echo "ZooKeeper server terminated successfully."
 ${BASE}/Kafka/bin/kafka-server-stop.sh
+echo "Kafka server terminated successfully."
