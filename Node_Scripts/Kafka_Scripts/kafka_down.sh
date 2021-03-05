607ec1df19d3f67a0784cb787e763628cd73ea81
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-03-05 10:57:04 -0700

    Update kafka_down.sh

--- Node_Scripts/Kafka_Scripts/kafka_down.sh
+++ Node_Scripts/Kafka_Scripts/kafka_down.sh
@@ -5,6 +5,16 @@ cd "${parent}"
 
 ../../Kafka/bin/kafka-server-stop.sh
 
+CHECK=1
+while [ check -ne 0 ]; do
+	tput setaf ${cnt}
+        echo -n "."
+        sleep 0.5
+	ps ax | grep 'kafka\.Kafka ' | grep java | grep -v grep | awk '{print $1}'
+	CHECK=$?
+done
+tput sgr0
+
 tput setaf 001
 if [[ $? -ne 0 ]]; then
 	echo "Failed to shut Kafka server down."
