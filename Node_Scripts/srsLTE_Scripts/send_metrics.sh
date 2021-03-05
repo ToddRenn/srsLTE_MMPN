89794508de140dbf54547cf1fa5fb24bca96c2f3
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-03-05 10:10:06 -0700

    Update send_metrics.sh

--- Node_Scripts/srsLTE_Scripts/send_metrics.sh
+++ Node_Scripts/srsLTE_Scripts/send_metrics.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 read -p "Node ID: " NODE_ID
-read -p "Kaf server: " server
+read -p "Kaf server: " KAF_IP
 
+server="--bootstrap-server ${KAF_IP}:9092"
 topic_csv="--topic ${NODE_ID}_csv"
 kaf_cmd="../../Kafka/bin/kafka-console-producer.sh ${topic_csv} ${server}"
 tail -f -n0 /tmp/ue_metrics.csv | ${kaf_cmd} &
