4db6be71bc45ac9e8d1231f84567039ee3237d49
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-03-05 10:40:16 -0700

    Update kafka_down.sh

--- Node_Scripts/Kafka_Scripts/kafka_down.sh
+++ Node_Scripts/Kafka_Scripts/kafka_down.sh
@@ -3,15 +3,6 @@
 parent=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 cd "${parent}"
 
-../../Kafka/bin/zookeeper-server-stop.sh
-tput setaf 001
-if [[ $? -ne 0 ]]; then
-	echo "Failed to shut Zookeeper down."
-else
-	echo "Zookeeper server DOWN."
-fi
-tput sgr0
-
 ../../Kafka/bin/kafka-server-stop.sh
 
 tput setaf 001
@@ -19,5 +10,14 @@ if [[ $? -ne 0 ]]; then
 	echo "Failed to shut Kafka server down."
 else
 	echo "Kafka server DOWN."
+fi
+tput sgr0
+
+../../Kafka/bin/zookeeper-server-stop.sh
+tput setaf 001
+if [[ $? -ne 0 ]]; then
+	echo "Failed to shut Zookeeper down."
+else
+	echo "Zookeeper server DOWN."
 fi
 tput sgr0
