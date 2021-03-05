39d6bb6ea692172b8c6e9d38752108f8e050b2c6
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-03-05 11:53:46 -0700

    Update kafka_down.sh

--- Node_Scripts/Kafka_Scripts/kafka_down.sh
+++ Node_Scripts/Kafka_Scripts/kafka_down.sh
@@ -4,38 +4,30 @@ parent=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 cd "${parent}"
 
 sudo ../../Kafka/bin/kafka-server-stop.sh
+pid_checker() kafka
 
-CHECK=0
-cnt=$(shuf -i 0-255 -n 1)
-while [ ${CHECK} -ne 1 ]; do
-	tput setaf ${cnt}
-        echo -n "."
-        sleep 0.5
-	ps ax | grep 'kafka\.Kafka ' | grep java | grep -v grep > /dev/null
-	CHECK=$?
-	if [[ ${cnt} -eq 0 ]]; then
-		cnt=255
-	else
-		(( cnt-- ))
-	fi
-done
-perl -e 'print "\xE2\x9C\x94 \xE2\x9C\x94 \xE2\x9C\x94 \xE2\x9C\x94"'
-echo ""
-tput sgr0
+sudo ../../Kafka/bin/zookeeper-server-stop.sh
+pid_checker() zookeeper
 
-tput setaf 001
-if [[ $? -ne 0 ]]; then
-	echo "Failed to shut Kafka server down."
-else
-	echo "Kafka server DOWN."
-fi
-tput sgr0
-
-../../Kafka/bin/zookeeper-server-stop.sh
-tput setaf 001
-if [[ $? -ne 0 ]]; then
-	echo "Failed to shut Zookeeper down."
-else
-	echo "Zookeeper server DOWN."
-fi
-tput sgr0
+pid_checker()
+{
+	CHECK=0
+	cnt=$(shuf -i 0-255 -n 1)
+	while [ ${CHECK} -ne 1 ]; do
+		tput setaf ${cnt}
+        	echo -n "."
+        	sleep 0.5
+		ps ax | grep ${1}.properties | grep -v grep > /dev/null
+		CHECK=$?
+		if [[ ${cnt} -eq 0 ]]; then
+			cnt=255
+		else
+			(( cnt-- ))
+		fi
+	done
+	perl -e 'print "\xE2\x9C\x94 \xE2\x9C\x94 \xE2\x9C\x94 \xE2\x9C\x94"'
+	echo ""
+	tput setaf 001
+	echo "${1} server DOWN."
+	tput sgr0
+}
