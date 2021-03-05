929109399cc94eb5a5ed87653c26a5e375c51937
root <root@node.t-renner-qv93181.mmpn.emulab.net>
2021-03-04 21:42:36 -0700

    kafka

--- Node_Scripts/Kafka_Scripts/delete_topics.sh
+++ Node_Scripts/Kafka_Scripts/delete_topics.sh
@@ -8,6 +8,6 @@ read -a topicName -p "DELETE: Enter topic name(s): "
 NODE_IP=$(ifconfig eno1 | grep -Po 'inet \K[\d.]+')
 
 for name in "${topicName[@]}"; do
-	echo "Creating topic: "${name}
+	echo "Deleting topic: "${name}
 	../../Kafka/bin/kafka-topics.sh --delete --topic ${name} --bootstrap-server ${NODE_IP}:9092
 done
