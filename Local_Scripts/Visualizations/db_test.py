ac281a8cb11cc09073fcab4413c2d8a880754290
TRenner <u1169633@utah.edu>
2021-03-12 18:10:12 -0500

    Initial influxDB python code

---
+++ Local_Scripts/Visualizations/db_test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+from datetime import datetime, timedelta
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# Setup basic parameters
+token = "gIDHwTgATjZcwUjUkdwhl5xCAs0YvxXQ3-FWaOYPOIPC074pekVBZx7C3qrKczea1h5M_Ua1uJpFH1qCGOusYQ=="
+org = "MMPN"
+bucket = "TEST"
+
+# Setup objects
+client = InfluxDBClient(url="http://localhost:8086", token=token)
+del_client = client.delete_api()
+# write options can set batch sizes so no need for .close() calls all
+# the time. it'll automatically do this I think.
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# Setup list of POINTS
+pts=[Point("point1")\
+     .tag("location","YMH")\
+     .tag("multiple",True)\
+     .field("you okay?","sure")\
+     .field("multifield","YEAP")\
+     .time(datetime.utcnow(),WritePrecision.NS),
+Point("point2").tag("location","helsinki").field("you okay?",False).time(datetime.utcnow(),WritePrecision.NS),
+Point("point3").tag("location","YMH").field("you okay?",1.0001).time(datetime.utcnow(),WritePrecision.NS)]
+
+# Write all these points at once. I suggest collecting in batches.
+write_api.write(bucket,org,pts)
+
+# Flush data
+write_api.close()
+
+# Query to see if data was actually written
+query = 'from(bucket:"TEST") |> range(start: -1h)'
+tables = client.query_api().query(query, org=org)
+print("Data creation display")
+for table in tables:
+    print(table)
+    for row in table.records:
+        print(row.values)
+
+# Delete all data
+del_client.delete('1970-01-01T00:00:00Z','2021-04-27T00:00:00Z','_measurement=point1',bucket=bucket,org=org)
+del_client.delete('1970-01-01T00:00:00Z','2021-04-27T00:00:00Z','_measurement=point2',bucket=bucket,org=org)
+del_client.delete('1970-01-01T00:00:00Z','2021-04-27T00:00:00Z','_measurement=point3',bucket=bucket,org=org)
+
+# Query again to verify deletion
+query = 'from(bucket:"TEST") |> range(start: -1h)'
+tables = client.query_api().query(query, org=org)
+print("Data deletion display")
+for table in tables:
+    print(table)
+    for row in table.records:
+        print(row.values)
+client.close()
+
