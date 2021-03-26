4ef1cf424910a3644c5ec5dbc6980dc021f57a3d
TRenner <u1169633@utah.edu>
2021-03-26 11:20:03 -0400

    Influx install fix

--- InfluxDB/influx_up.sh
+++ InfluxDB/influx_up.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Adding influx/influxd to \$PATH..."
-sudo cp influxdb2-2.0.4-linux-amd64/{influx,influxd} /usr/local/bin && echo -n "SUCCESS" || echo -n "FAILED"
+echo -n "Adding influx/influxd to \$PATH..."
+sudo cp influxdb2-2.0.4-amd64/{influx,influxd} /usr/local/bin && echo "SUCCESS" || echo "FAILED"
 
-echo "Installing .deb file..."
-sudo dpkg -i influxdb2_2.0.4-amd64.deb && echo -n "SUCCESS" || echo -n "FAILED"
+echo -n "Installing .deb file..."
+sudo dpkg -i influxdb2_2.0.4-amd64.deb && echo "SUCCESS" || echo "FAILED"
 
-echo "Starting InfluxDB..."
-sudo service influxdb start && echo -n "SUCCESS" || echo -n "FAILED"
+echo -n "Starting InfluxDB..."
+sudo service influxdb start && echo "SUCCESS" || echo "FAILED"
 
 
