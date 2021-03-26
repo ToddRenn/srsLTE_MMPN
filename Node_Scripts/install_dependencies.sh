382bc456435776cc34a4de21b8d658e756bf22c4
TRenner <u1169633@utah.edu>
2021-03-26 11:30:37 -0400

    Merged influx into dependencies install

--- Node_Scripts/install_dependencies.sh
+++ Node_Scripts/install_dependencies.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+parent=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+cd "${parent}"
+
+# Update & Install Java
 sudo apt-get update
 sudo apt-get install default-jre -y
+
+# Install/configure InfluxDB
+echo -n "Adding influx/influxd to \$PATH..."
+sudo cp ../InfluxDB/influxdb2-2.0.4-linux-amd64/{influx,influxd} \
+	/usr/local/bin && echo "SUCCESS" || echo "FAILED"
+echo -n "Installing .deb file..."
+sudo dpkg -i ../InfluxDBinfluxdb2-2.0.4-amd64.deb && \
+	echo "SUCCESS" || echo "FAILED"
+echo -n "Starting InfluxDB..."
+sudo service influxdb start && echo "SUCCESS" || echo "FAILED"
+
 
