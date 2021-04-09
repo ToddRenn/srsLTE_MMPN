75aafe498bdeaa6a245169dcff6e7f7ece560b8f
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-04-09 10:28:31 -0600

    Update install_dependencies.sh
    optional influxdb install

--- Node_Scripts/install_dependencies.sh
+++ Node_Scripts/install_dependencies.sh
@@ -3,18 +3,22 @@
 parent=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 cd "${parent}"
 
+read -p "Include InfluxDB (y/n)? " ANS
+
 # Update & Install Java
 sudo apt-get update
 sudo apt-get install default-jre -y
 
-# Install/configure InfluxDB
-echo -n "Adding influx/influxd to \$PATH..."
-sudo cp ../InfluxDB/influxdb2-2.0.4-linux-amd64/{influx,influxd} \
-	/usr/local/bin && echo "SUCCESS" || echo "FAILED"
-echo -n "Installing .deb file..."
-sudo dpkg -i ../InfluxDB/influxdb2-2.0.4-amd64.deb && \
-	echo "SUCCESS" || echo "FAILED"
-echo -n "Starting InfluxDB..."
-sudo service influxdb start && echo "SUCCESS" || echo "FAILED"
-
+if [[ ${ANS} = "y" ]]
+then
+	# Install/configure InfluxDB
+	echo -n "Adding influx/influxd to \$PATH..."
+	sudo cp ../InfluxDB/influxdb2-2.0.4-linux-amd64/{influx,influxd} \
+		/usr/local/bin && echo "SUCCESS" || echo "FAILED"
+	echo -n "Installing .deb file..."
+	sudo dpkg -i ../InfluxDB/influxdb2-2.0.4-amd64.deb && \
+		echo "SUCCESS" || echo "FAILED"
+	echo -n "Starting InfluxDB..."
+	sudo service influxdb start && echo "SUCCESS" || echo "FAILED"
+fi
 
