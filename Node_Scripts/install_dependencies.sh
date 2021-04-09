04a3b34956a4ab09f8c5a5b6e54c642055cf23c3
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-04-09 10:19:07 -0600

    Update install_dependencies.sh

--- Node_Scripts/install_dependencies.sh
+++ Node_Scripts/install_dependencies.sh
@@ -12,7 +12,7 @@ echo -n "Adding influx/influxd to \$PATH..."
 sudo cp ../InfluxDB/influxdb2-2.0.4-linux-amd64/{influx,influxd} \
 	/usr/local/bin && echo "SUCCESS" || echo "FAILED"
 echo -n "Installing .deb file..."
-sudo dpkg -i ../InfluxDBinfluxdb2-2.0.4-amd64.deb && \
+sudo dpkg -i ../InfluxDB/InfluxDBinfluxdb2-2.0.4-amd64.deb && \
 	echo "SUCCESS" || echo "FAILED"
 echo -n "Starting InfluxDB..."
 sudo service influxdb start && echo "SUCCESS" || echo "FAILED"
