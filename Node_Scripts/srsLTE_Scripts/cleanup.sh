200e22e18c1f9bfbd96b73ffd041aeec343c7655
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2021-04-25 07:48:52 -0600

    Update cleanup.sh
    Expanded functionality for all nodes

--- Node_Scripts/srsLTE_Scripts/cleanup.sh
+++ Node_Scripts/srsLTE_Scripts/cleanup.sh
@@ -6,6 +6,12 @@
 for pid in $(ps ax|grep srsenb|grep -v grep|awk '{print $1}'); do
 	sudo kill -9 $pid
 done
+for pid in $(ps ax|grep srsepc|grep -v grep|awk '{print $1}'); do
+	sudo kill -9 $pid
+done
+for pid in $(ps ax|grep srsue|grep -v grep|awk '{print $1}'); do
+	sudo kill -9 $pid
+done
 for pid in $(ps ax|grep ConsoleProducer|grep -v grep|awk '{print $1}'); do
 	sudo kill -9 $pid
 done
